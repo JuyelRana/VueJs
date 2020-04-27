31107f5fee802248ed1a65876658a209f88652ea
Juyel <mjuyelrana@gmail.com>
2020-04-28 00:15:43 +0600

    Text and style

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1>About page</h1>
+    
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <h1>Home Page</h1>
+    <p class="primary red--text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p class="indigo lighten-4 indigo--text lighten-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    <h1 class="display-4 text-uppercase">Massive Display</h1>
+    <h4 class="display-1 text-lowercase">Smaller Display</h4>
+    <p class="headline text-capitalize">This is headline</p>
+    <p class="subheading font-weight-bold">This is sub headline</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
 
