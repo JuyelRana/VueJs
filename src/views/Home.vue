307cecd225b238522e942b6e1d220238b6a2abb8
Juyel <mjuyelrana@gmail.com>
2020-04-28 00:55:37 +0600

    Buttons Styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,125 @@
 <template>
   <div>
     <h1>Home Page</h1>
-    <p class="primary red--text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p class="indigo lighten-4 indigo--text lighten-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-    <h1 class="display-4 text-uppercase">Massive Display</h1>
-    <h4 class="display-1 text-lowercase">Smaller Display</h4>
-    <p class="headline text-capitalize">This is headline</p>
-    <p class="subheading font-weight-bold">This is sub headline</p>
-    <p class="caption">This is a caption</p>
+    <v-btn rounded class="indigo white--text">click me </v-btn>
+    <v-btn outlined color="pink">Click Me</v-btn>
+
+    <v-btn raised color="indigo white--text">Click Me</v-btn>
+
+    <v-btn block color="pink">Click Me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>Email Me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>mdi-email</v-icon>
+      <span>Email Me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>Email Me</span>
+      <v-icon right large>mdi-email</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <div class="text-center">
+      <v-btn class="mx-2" fab dark small color="primary">
+        <v-icon dark>mdi-minus</v-icon>
+      </v-btn>
+
+      <v-btn class="mx-2" fab dark small color="pink">
+        <v-icon dark>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn class="mx-2" fab dark color="indigo">
+        <v-icon dark>mdi-plus</v-icon>
+      </v-btn>
+
+      <v-btn class="mx-2" fab dark color="teal">
+        <v-icon dark>mdi-format-list-bulleted-square</v-icon>
+      </v-btn>
+
+      <v-btn class="mx-2" fab dark large color="cyan">
+        <v-icon dark>mdi-pencil</v-icon>
+      </v-btn>
+
+      <v-btn class="mx-2" fab dark large color="purple">
+        <v-icon dark>mdi-pen</v-icon>
+      </v-btn>
+    </div>
+
+    <v-card flat>
+      <v-card-text>
+        <v-container fluid class="pa-0">
+          <v-row>
+            <v-col cols="12">
+              <p>Normal</p>
+            </v-col>
+
+            <v-col cols="12" sm="3">
+              <v-btn icon color="pink">
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+            </v-col>
+
+            <v-col cols="12" sm="3">
+              <v-btn icon color="indigo">
+                <v-icon>mdi-star</v-icon>
+              </v-btn>
+            </v-col>
+
+            <v-col cols="12" sm="3">
+              <v-btn icon color="green">
+                <v-icon>mdi-cached</v-icon>
+              </v-btn>
+            </v-col>
+
+            <v-col cols="12" sm="3">
+              <v-btn icon color="deep-orange">
+                <v-icon>mdi-thumb-up</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+
+          <v-row class="mt-12">
+            <v-col cols="12">
+              <p>Disabled</p>
+            </v-col>
+
+            <v-col cols="12" sm="3">
+              <v-btn icon disabled>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+            </v-col>
+
+            <v-col cols="12" sm="3">
+              <v-btn icon disabled>
+                <v-icon>mdi-star</v-icon>
+              </v-btn>
+            </v-col>
+
+            <v-col cols="12" sm="3">
+              <v-btn icon disabled>
+                <v-icon>mdi-cached</v-icon>
+              </v-btn>
+            </v-col>
+
+            <v-col cols="12" sm="3">
+              <v-btn icon disabled>
+                <v-icon>mdi-thumb-up</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+    </v-card>
+
   </div>
 </template>
 
